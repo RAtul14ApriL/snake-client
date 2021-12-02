9b77c175a0b38664d8ad99c7d9ca00bb9f5d0a11
Md Hefzul Bari <ratul14april@gmail.com>
2021-12-01 19:13:27 -0600

    Playing Snake

--- play.js
+++ play.js
@@ -2,3 +2,30 @@ const {connect} = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w'){
+    process.stdin("Move: up");
+  }
+  if (key === 's'){
+    process.stdin("Move: down");
+  }
+  if (key === 'a'){
+    process.stdin("Move: left");
+  }
+  if (key === 'd'){
+    process.stdin("Move: right");
+  }
+};
