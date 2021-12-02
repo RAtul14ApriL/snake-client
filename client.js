93dd0dd1f63f85f5635db4c74488c079c44e51e7
Md Hefzul Bari <ratul14april@gmail.com>
2021-12-01 18:35:48 -0600

    Playing Snake

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin
+// });
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on('connect', (connect) => {
+    console.log('King Cobra', connect);
+    conn.write("Name: RAT");
+  })
+
+  return conn;
+}
+
+module.exports = { connect }
